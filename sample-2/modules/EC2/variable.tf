bb8189ebfce8aff2c4579740bff783f762a32fe7
ArunachalamG23 <arunachalamg2303@gmail.com>
2025-01-28 11:36:43 +0000

    Script for modules

---
+++ sample-2/modules/EC2/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+  description = "value of ami id"
+}
+variable "instance_type" {
+  description = "value for instance type"
+}
+variable "subnet_id" {
+  description = "value for subnet id"
+}
+
